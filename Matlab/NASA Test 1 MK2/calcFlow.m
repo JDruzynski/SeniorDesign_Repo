c39bb7fc14656b864fc96c7d7e5fa475c1dd4ab9
Jarred Druzynski <jarred.druzynski@gmail.com>
2020-10-01 14:16:51 -0700

    UPdate 10/1/2020

--- Matlab/NASA Test 1 MK2/calcFlow.m
+++ Matlab/NASA Test 1 MK2/calcFlow.m
@@ -1,4 +1,4 @@
-function flow=calcFlow(setPoint, UnitID)
+function flow=calcFlow(setPoint, fullScale)
 % function flow=calcFlow(setPoint)
 %
 % Convert a desired flow rate into the correct units for the MFC. See p. 22
@@ -9,12 +9,7 @@ function flow=calcFlow(setPoint, UnitID)
 
 %flow = (setPoint * 64000)/5; %64000/5 for MFC E
 
-if UnitID == 'A'|| UnitID == 'B'
-    flow = (setPoint * 64000)/50; %ISA50 and ISB50 %64000/50 for MFC A and B
-elseif UnitID == 'C'||UnitID == 'D'
-    flow = (setPoint * 64000)/1; %ISA1 and ISAB1 %64000/1 for MFC C and D
-else %if UnitID is E
-    flow = (setPoint * 64000)/5; %64000/5 for MFC E
+flow = (setPoint * 64000)/fullScale;
 end
  
  
